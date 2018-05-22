fbf228efb49e3d2518bd2834316b1484aaee006c
zhanglei <zhanglei>
2018-05-22 14:41:34 +0800

    init

---
+++ src/main/java/com/jd/fabric/FabricDeployApplication.java
@@ -0,0 +1,12 @@
+package com.jd.fabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricDeployApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jd/fabric/FabricDeployApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jd.fabric;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FabricDeployApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
